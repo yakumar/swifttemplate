8989c00c730b3ce7d14ed27858c90c9fccec7481
yakumar <yakumar_in@yahoo.com>
2022-08-25 11:37:59 +0530

    Initial Commit

---
+++ LoanCD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoanCD
+//
+//  Created by ani on 25/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoanCD/LoanCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanCDApp.swift
+//  LoanCD
+//
+//  Created by ani on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LoanCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
